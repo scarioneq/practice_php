c3cb2ef6d221d623c2e0a831b5e7579c7e9e6cfb
Евгений Киенко <gr943_kes@lab-203-9.tomtit-tomsk.ru>
2026-04-08 09:53:24 +0700

    Разработали классы для аутентификации

--- pop-it-mvc/app/Controller/Site.php
+++ pop-it-mvc/app/Controller/Site.php
@@ -4,6 +4,7 @@ namespace Controller;
 
 use Model\Post;
 use Model\User;
+use Src\Auth\Auth;
 use Src\Request;
 use Src\View;
 
@@ -26,8 +27,29 @@ class Site
     public function signup(Request $request): string
     {
         if ($request->method === 'POST' && User::create($request->all())) {
+            Auth::attempt($request->all());
             app()->route->redirect('/go');
         }
         return new View('site.signup');
+    }
+
+    public function login(Request $request): string
+    {
+        //Если просто обращение к странице, то отобразить форму
+        if ($request->method === 'GET') {
+            return new View('site.login');
+        }
+        //Если удалось аутентифицировать пользователя, то редирект
+        if (Auth::attempt($request->all())) {
+            app()->route->redirect('/hello');
+        }
+        //Если аутентификация не удалась, то сообщение об ошибке
+        return new View('site.login', ['message' => 'Неправильные логин или пароль']);
+    }
+
+    public function logout(): void
+    {
+        Auth::logout();
+        app()->route->redirect('/hello');
     }
 }
